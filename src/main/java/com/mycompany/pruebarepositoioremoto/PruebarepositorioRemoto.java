ad5d81c2cd905772389ad1bff53997b6709f7f05
Usuario Toshiba <80179255+emelysantosmejia@users.noreply.github.com>
2021-03-08 19:52:32 -0600

    Primer comnit para repositorio remmoto -

---
+++ src/main/java/com/mycompany/pruebarepositoioremoto/PruebarepositorioRemoto.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pruebarepositoioremoto;
+
+/**
+ *
+ * @author Usuario Toshiba
+ */
+public class PruebarepositorioRemoto {
+    
+    /**
+     * 
+     * @param args 
+     */
+    
+    public static void main (String[] args){
+        //TODO code :application logic hare
+        
+       System.out.println("vamos a guardar el codigo en GitHub");
+    }
+}
